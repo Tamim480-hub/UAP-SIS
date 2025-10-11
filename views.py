7162b645c24d68580d012515a6f980eb8d99df06
23101215-art <23101215@uap-bd.edu>
2025-10-11 15:19:48 +0600

    Delete views.py

--- views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import ExamRoutine
-
-def exam_routine_view(request):
-    routines = ExamRoutine.objects.all().order_by('date', 'time')
-    return render(request, 'exam/exam_routine.html', {'routines': routines})
